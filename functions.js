b2c8e4177a80d7318fedea7e21c50d2a23da95c5
Student <yvonneoyellaa@gmail.com>
2024-03-29 19:50:41 +0300

    made changes

--- functions.js
+++ functions.js
@@ -1,6 +1,9 @@
 //1.Given an array of strings, use a function to reverse all the elements in the string in ascending order and the specific elements in descending order
 
-
+// function
+  
+//   const stringsArray = ['What', 'a', 'wonderful', 'world'];
+//   console.log(reverseStrings(stringsArray)); 
 
 
 
@@ -8,6 +11,14 @@
 
 //2.Using JS functions and an array of numbers, return positive if an element within the array is positive, negative if an element is negative, else zero
 
+function checkSign(arr) {
+    const positive = arr.nums(num => num > 0);
+    const negative = arr.nums(num => num < 0);
+    return positive ? 'positive' : (negative ? 'negative' : 'zero');
+  }
+  
+  const numbersArray = [4, -8, 10, 0, -2];
+  console.log(checkSign(numbersArray)); 
 
 
 
@@ -16,20 +27,33 @@
 
 function sortEmployeesBySalary(employees) {
     return employees.sort((a, b) => a.salary - b.salary);
-    employees(23,"Oyella",20)  }
+  }
+  
+  const employeesArray = [
+    { id: 1, name: 'John', salary: 500000 },
+    { id: 2, name: 'Alice', salary: 600000 },
+    { id: 3, name: 'Bob', salary: 450000 }
+  ];
+  console.log(sortEmployeesBySalary(employeesArray));
+ 
+
 
 
 //4.Write a function that accepts an array of numbers and uses the forEach() method to console.log each number multiplied by 2.
 
+function multiplyByTwo(arr) {
+    arr.forEach(num => console.log(num * 2));
+  }
+  
+  const numbersArray2 = [10, 20, 30, 40, 50];
+  multiplyByTwo(numbersArray2);
 
-/
-//   arr.forEach(num => console.log(num * 2));
-// }
-// const num= 
 
 
 //5.Write a function that takes in an array of numbers and consoles the first four items multiplied by 8 and the last two added by 5. Console the array with the new values
 
-// const newArr = [...arr];
-//   newArr.slice(0, 4).forEach(num => console.log(num * 8));
-//   newArr.slice(-2).forEach(num => console.log(num + 5));
+function changeArray(arr) {
+    const newArr = [arr];
+    newArr.slice(0, 4).forEach(num => console.log(num * 8));
+    newArr.slice(-2).forEach(num => console.log(num + 5));
+  }
